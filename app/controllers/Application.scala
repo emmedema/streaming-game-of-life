871edd6e8ee43c3170c0fb282687b3c80ca5f010
Matteo De Martino <emmedema@gmail.com>
2014-09-13 15:21:13 +0100

    UI can request updates

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,7 @@
 package controllers
 
-import play.api._
-import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Application extends Controller {
 
@@ -10,8 +9,8 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  import play.api.mvc._
   import play.api.Play.current
+  import play.api.mvc._
 
   def socket = WebSocket.acceptWithActor[String, String] { request => out =>
     MyWebSocketActor.props(out)
@@ -20,16 +19,33 @@ object Application extends Controller {
   import akka.actor._
 
   object MyWebSocketActor {
-    def props(out: ActorRef) = Props(new MyWebSocketActor(out))
+    def props(out: ActorRef) = Props(new MyWebSocketActor(out, 50))
   }
 
-  class MyWebSocketActor(out: ActorRef) extends Actor {
+  class MyWebSocketActor(out: ActorRef, rowSize : Int) extends Actor {
+    val boardSize = rowSize * rowSize
+    var sotSub : Cancellable = null
+    var sot : String = null
+
+    override def postStop = cancelUpdates
+
     def receive = {
       case "sot" => {
         println("Requested the state of the world")
 
-        //TODO
-        out ! getStateOfTheWorld
+        sot = getStateOfTheWorld
+        out ! sot
+
+        cancelUpdates
+
+        import scala.concurrent.duration._
+        sotSub = context.system.scheduler.schedule(0 millis, 2 second, self, "shift")
+      }
+      case "shift" => {
+        out ! shiftStateOfTheWorld
+      }
+      case "end" => {
+        self ! PoisonPill
       }
       case _ => {
         println("Unhandled message. Killing ws.")
@@ -38,8 +54,21 @@ object Application extends Controller {
       }
     }
 
-    def getStateOfTheWorld : String = {
-      "0" * 2500
+    def cancelUpdates = if(sotSub != null && !sotSub.isCancelled) sotSub.cancel()
+
+    def getStateOfTheWorld: String = {
+      val sb = new StringBuilder(boardSize).append("0"*boardSize)
+      sb.setCharAt(0, '1')
+      sb.toString()
+    }
+
+    def shiftStateOfTheWorld: String = {
+      val sb = new StringBuilder(sot)
+      val i = sot.indexOf("1")
+      sb.setCharAt(i, '0')
+      sb.setCharAt((i + 1) % boardSize, '1')
+      sot = sb.toString()
+      sot
     }
   }
 
